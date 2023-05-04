cc15dd0842789635733b9879d3bac8a61a3cc9e8
Motblunt <theo.cottin@gmail.com>
2023-05-04 14:57:50 +0200

    add component NotFound

---
+++ projet-kasa/src/components/NotFount/Notfound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Banner from '../Banner/Banner';
+import "../NotFount/Notfound.css"
+
+const Notfound = () => {
+    return (
+        <div >
+            <Banner />
+            <div className='display-flex'>
+                <h1>404</h1>
+                <h2>Oups! La page que vous demandez n'existe pas.</h2>
+                <p><NavLink className={"active-nav"} to="/">Retourner sur la page d’accueil</NavLink></p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Notfound;
