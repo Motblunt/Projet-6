5b4d407467590f980ad61ea93eb133b6318e6e6b
Motblunt <theo.cottin@gmail.com>
2023-05-06 12:04:43 +0200

    add component carousel

--- projet-kasa/src/components/CardPage/Cardpage.js
+++ projet-kasa/src/components/CardPage/Cardpage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import cardsData from '../data.json';
 import Notfound from '../NotFount/Notfound';
 import Fichelogement from '../FicheLogement/Fichelogement';
+import Banner from '../Banner/Banner';
 
 const CardPage = () => {
   const { cardId } = useParams();  
@@ -17,6 +18,7 @@ const card = cardsData.find((card) => card.id === formattedCardId);
 
   return (
     <div>
+      <Banner/>
       <Fichelogement/>
     </div>
   );
--- projet-kasa/src/components/FicheLogement/Fichelogement.js
+++ projet-kasa/src/components/FicheLogement/Fichelogement.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import cardsData from '../data.json';
 import { useParams } from 'react-router-dom';
-
+import Carousel from '../Carousel/Carousel';
+import '../FicheLogement/Fichelogement.css';
 
 const FicheLogement = () => {
-    const { cardId } = useParams();  
-    const formattedCardId = cardId.includes("_") ? cardId.slice(cardId.indexOf("_") + 1) : cardId;
-  const card = cardsData.find((card) => card.id === formattedCardId);  
+  const { cardId } = useParams();
+  const formattedCardId = cardId.includes('_') ? cardId.slice(cardId.indexOf('_') + 1) : cardId;
+  const card = cardsData.find((card) => card.id === formattedCardId);
 
-
-    return (
-        <div>
-            <h2>{card.title}</h2>
-            <p>{card.description}</p>
+  return (
+    <div>
+      <Carousel images={card.pictures} />
+      <div className="main-container">
+        <div className="container-title">
+          <h1>{card.title}</h1>
+          <p>{card.location}</p>
         </div>
-    );
+        <div className="container-host">
+          <span>{card.host.name}</span>
+          <img src={card.host.picture} alt={card.host.name} />
+        </div>
+        </div>
+        <div>
+          <div className="tags-name">
+            {card.tags.map((tag) => (
+              <button key={tag} className="tag-button">
+                {tag}
+              </button>
+            ))}
+          </div>
+        </div>
+      
+    </div>
+  );
 };
 
 export default FicheLogement;
